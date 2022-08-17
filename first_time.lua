2de0184cd308d5005b2390cf7c12731c8bfaa35a
Lisso-lab <85261069+Lisso-lab@users.noreply.github.com>
2022-08-17 14:10:33 +0000

    Create first_time.lua

---
+++ first_time.lua
@@ -0,0 +1,30 @@
+local commands = {
+    "noclip.lua"
+}
+
+local url = "https://raw.githubusercontent.com/Lisso-lab/Admin-stuff/main/"
+
+local signats = loadstring(game:HttpGet(url .. "signatures.lua"))()
+
+url = url .. "soadmin/"
+
+local function load_url(str: string)
+    return game:HttpGet(url .. str)
+end
+
+makefolder("/soadmin")
+makefolder("/soadmin/commands")
+
+writefile("/soadmin/main.lib", load_url("main.lib"))
+
+writefile("/soadmin/settings.json", "") --TODO :SOB:
+
+for i=1,#commands do
+    writefile("/soadmin/commands/" .. commands[i], load_url("commands/" .. commands[i]))
+
+    signats.make_signature("/soadmin/commands/" .. commands[i])
+end
+
+signats.make_signature("/soadmin/main.lib")
+
+signats.make_signature("/soadmin/settings.json")
