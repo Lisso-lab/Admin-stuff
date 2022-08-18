1ab50d4960c84d0aeb65dc2f53ab4fc0d8331243
Lisso-lab <85261069+Lisso-lab@users.noreply.github.com>
2022-08-18 09:33:58 +0000

    Update noclip.lua

--- soadmin/commands/noclip.lua
+++ soadmin/commands/noclip.lua
@@ -1,1 +1,24 @@
+--!strict
+return {"nclip","noclip","collide"},"Disabled collision of Player", function()
+    local run_service: RunService = game:GetService("RunService")
 
+    if cmd_props.noclip then
+        cmd_props.noclip:Disconnect()
+
+        cmd_props.noclip = nil
+	
+	notify("Disabled noclip")
+    else
+        cmd_props.noclip = run_service["Stepped"]:Connect(function()
+            local char = get_char()
+
+            if char then
+                for _,v in pairs(char:GetDescendants()) do
+                    if v:IsA("BasePart") then v.CanCollide = false end
+                end
+            end
+        end)
+
+	notify("Enabled noclip")
+    end
+end
